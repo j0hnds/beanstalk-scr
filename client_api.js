634960c3a3095e4544da7f57a057b0e2ede78cf3
Dave <dsieh@providigm.com>
2016-10-27 14:06:17 -0600

    Added command line parsing.

--- client_api.js
+++ client_api.js
@@ -2,22 +2,41 @@
 
 const Client = require('node-rest-client').Client
 
-const client = new Client();
-
-const url = 'https://www.abaqis.com/beanstalk'
+const client = new Client()
 
 const headers = {
   Authorization: 'Token token="' + process.env.API_TOKEN + '"'
 }
 
-const get = (uri, args) => {
-  return new Promise((resolve, reject) => {
-    client.get(url + uri, { data: args, headers: headers }, (data, response) => {
-      resolve(data)
+class ClientAPI {
+  constructor (server, port, token, nonencrypted) {
+    this._server = server
+    this._port = port
+    this._token = token
+    this._nonencrypted = nonencrypted
+  }
+
+  _buildUrl (uri) {
+    let url = (this._nonencrypted) ? 'http://' : 'https://'
+
+    url += this._server
+
+    if (this._port) url += `:${this._port}`
+
+    url += '/beanstalk'
+
+    console.log('URL: %s', url + uri)
+    return url + uri
+  }
+
+  get (uri, args) {
+    return new Promise((resolve, reject) => {
+      client.get(this._buildUrl(uri), { data: args, headers: headers }, (data, response) => {
+        resolve(data)
+      })
     })
-  })
+  }
+
 }
 
-module.exports = {
-  get: get
-}
+module.exports.ClientAPI = ClientAPI
--- index.js
+++ index.js
@@ -2,41 +2,55 @@
 
 'use strict'
 const blessed = require('blessed')
-const clientApi = require('./client_api')
+const ClientAPI = require('./client_api').ClientAPI
+const program = require('commander')
+
+program
+  .version('1.0.0')
+  .option('-s,--server [server]',
+          `Server for beanstalk_api [${process.env.API_SERVER}]`,
+          process.env.API_SERVER)
+  .option('-p,--port [port]',
+          'Server port for beanstalk_api')
+  .option('-t,--token [token]',
+          `API token [${process.env.API_TOKEN}]`,
+          process.env.API_TOKEN)
+  .option('-n,-nonencrypted',
+          'Toggles encrypted request',
+          false)
+  .parse(process.argv)
+
+const clientApi = new ClientAPI(program.server, program.port, program.token, program.nonencrypted)
 
 // Create a screen object
 const screen = blessed.screen({ smartCSR: true })
 
 screen.title = 'Beanstalk API'
 
-const menu = blessed.listbar({
+blessed.listbar({
   parent: screen,
   mouse: true,
   keys: true,
   commands: {
     Invoke: {
       keys: [ 'i' ],
-      callback: () => { 
-        clientApi.get('', {}).
-          then((data) => {
+      callback: () => {
+        clientApi.get('', {})
+          .then((data) => {
             let hdr = [ [ 'Attribute', 'Value' ] ]
-						let arr = [
-        			[ 'current_jobs_ready', '' + data['current_jobs_ready'] ],
-       				[ 'current_jobs_urgent', '' + data['current_jobs_urgent'] ],
-     					[ 'current_jobs_reserved', '' + data['current_jobs_reserved'] ],
-      				[ 'current_jobs_delayed', '' + data['current_jobs_delayed'] ],
-       				[ 'current_jobs_buried', '' + data['current_jobs_buried'] ],
+            let arr = [
+              [ 'current_jobs_ready', '' + data['current_jobs_ready'] ],
+              [ 'current_jobs_urgent', '' + data['current_jobs_urgent'] ],
+              [ 'current_jobs_reserved', '' + data['current_jobs_reserved'] ],
+              [ 'current_jobs_delayed', '' + data['current_jobs_delayed'] ],
+              [ 'current_jobs_buried', '' + data['current_jobs_buried'] ],
               [ 'job_timeouts', '' + data['job_timeouts'] ],
               [ 'current_tubes', '' + data['current_tubes'] ],
               [ 'current_connections', '' + data['current_connections'] ],
               [ 'current_workers', '' + data['current_workers'] ],
               [ 'current_waiting', '' + data['current_waiting'] ],
-              [ 'total_jobs', '' + data['total_jobs'] ],
+              [ 'total_jobs', '' + data['total_jobs'] ]
             ]
-            //let arr = Object.keys(data).map((currentValue, index, arr) => { 
-              //return [ currentValue, 
-                //'' + data[currentValue] ] 
-            //})
             results.setData(hdr.concat(arr))
             tubeList.setData([])
             screen.render()
@@ -46,8 +60,8 @@ const menu = blessed.listbar({
     Tubes: {
       keys: [ 't' ],
       callback: () => {
-        clientApi.get('/tubes', {}).
-          then((data) => {
+        clientApi.get('/tubes', {})
+          .then((data) => {
             let hdr = [ [ 'Tubes' ] ]
             let arr = data.map((currentValue, index, arr) => {
               return [ currentValue ]
@@ -81,7 +95,7 @@ const tubeList = blessed.listtable({
       },
       hover: {
         bg: 'green'
-      },
+      }
     },
     header: {
       bold: true
@@ -90,16 +104,16 @@ const tubeList = blessed.listtable({
       bg: 'red',
       fg: 'blue'
     }
-  },
+  }
 })
 
-function urlTubeName(tubeName) {
+function urlTubeName (tubeName) {
   return new Buffer(tubeName).toString('base64')
 }
 
-tubeList.on('select', (data, index) => { 
-  clientApi.get('/tubes/' + urlTubeName(data.content.trim()), {}).
-    then((data) => {
+tubeList.on('select', (data, index) => {
+  clientApi.get('/tubes/' + urlTubeName(data.content.trim()), {})
+    .then((data) => {
       // console.log("Data: %j", data)
       let hdr = [ [ 'Attribute', 'Value' ] ]
       let arr = [
@@ -111,10 +125,6 @@ tubeList.on('select', (data, index) => {
         [ 'current_waiting', '' + data['current_waiting'] ],
         [ 'total_jobs', '' + data['total_jobs'] ]
       ]
-      //let arr = Object.keys(data).map((currentValue, index, arr) => { 
-        //return [ currentValue, 
-          //'' + data[currentValue] ] 
-      //})
       results.setData(hdr.concat(arr))
       screen.render()
     })
@@ -137,12 +147,7 @@ const results = blessed.listtable({
       bg: 'red',
       fg: 'blue'
     }
-  },
-  //rows: [
-    //[ 'Attribute', 'Value' ]
-  //]
+  }
 })
 
 screen.render()
-//clientApi.get({})
-  //.then((data) => console.log("Data: %j", data))
